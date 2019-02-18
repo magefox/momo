25668139c601ff3fec01e13cf8a3b451e64c58bb
Magefox <magefoxtech@gmail.com>
2019-02-18 10:45:15 +0700

    Initial commit

---
+++ registration.php
@@ -0,0 +1,13 @@
+<?php
+/******************************************************
+ * @package Magento 2 MoMo Payment Gateway Integration
+ * @author http://www.magefox.com
+ * @copyright (C) 2019 - Magefox.Com
+ * @license MIT
+ *******************************************************/
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Magefox_MoMo',
+    __DIR__
+);
